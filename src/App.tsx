1e3dc4e8f769b404508000eb8bc5bc10af36030e
Binh <nt.binh21@gmail.com>
2021-06-18 15:22:02 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import parse from "html-react-parser";
+import "./App.css";
 
 function App() {
-
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState < string > "";
   const [listPackage, setListPackage] = useState<any[]>([]);
 
   const callApi = async (keyword: string) => {
@@ -11,9 +11,9 @@ function App() {
       const url = `https://fast-npm-search.xyz/packages?input=${keyword}`;
       const res = await fetch(url);
       const data = await res.json();
-      setListPackage(data.packages)
+      setListPackage(data.results);
     } catch (e) {
-      throw e
+      throw e;
     }
   };
 
@@ -22,52 +22,58 @@ function App() {
       callApi(value);
       setValue(value);
     } else {
-      setValue('');
+      setValue("");
       setListPackage([]);
     }
-
   };
 
   return (
-      <div className="instantSearch">
-        <div className="instantSearch__input">
-          <input type="text" placeholder="Search..." className="form-control" value={value}
-                 onChange={event => {
-                   handleSearch(event.target.value)
-                 }}/>
-        </div>
-        <div className="instantSearch__main">
-          <div className="instantSearch__row">
-            {
-              listPackage.length > 0 && listPackage.map((item: any) => {
-                return <div className="instantSearch__col" key={item.id}>
+    <div className="instantSearch">
+      <div className="instantSearch__input">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="form-control"
+          value={value}
+          onChange={(event) => {
+            handleSearch(event.target.value);
+          }}
+        />
+      </div>
+      <div className="instantSearch__main">
+        <div className="instantSearch__row">
+          {listPackage.length > 0 &&
+            listPackage.map((item: any) => {
+              return (
+                <div className="instantSearch__col" key={item.package.id}>
                   <div className="instantSearch__item">
                     <ul>
-                      <li className='d-flex pb-10'>
-                        <span className='bold black-1 mr-15'>name:</span>
-                        <span className="medium black-1 flex1">{item.name}</span>
-                      </li>
-                      <li className='d-flex pb-10'>
-                        <span className='bold black-1 mr-15'>description:</span>
-                        <span className="medium black-1 flex1">{item.description}</span>
-                      </li>
-                      <li className='d-flex pb-10'>
-                        <span className='bold black-1 mr-15'>downloads:</span>
-                        <span className="medium black-1 flex1">{item.downloads}</span>
-                      </li>
-                      <li className='d-flex pb-10'>
-                        <span className='bold black-1 mr-15'>id:</span>
-                        <span className="medium black-1 flex1">{item.id}</span>
+                      {item.highlights.map((highlight: any) => {
+                        return (
+                          <li className="d-flex pb-10" key={highlight.field}>
+                            <span className="bold black-1 mr-15">
+                              {highlight.field}:
+                            </span>
+                            <span className="medium black-1 flex1">
+                              {parse(highlight.value)}
+                            </span>
+                          </li>
+                        );
+                      })}
+                      <li className="d-flex pb-10">
+                        <span className="bold black-1 mr-15">description:</span>
+                        <span className="medium black-1 flex1">
+                          {item.package.description}
+                        </span>
                       </li>
                     </ul>
                   </div>
                 </div>
-              })
-            }
-          </div>
+              );
+            })}
         </div>
       </div>
-
+    </div>
   );
 }
 
